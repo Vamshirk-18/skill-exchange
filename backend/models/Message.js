a76fb99813eacb3423d835675df91aab80ccf5fa
Vamshirk-18 <vamshirk05@gmail.com>
2026-06-30 17:54:20 +0530

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
   swapRequest: { type: mongoose.Schema.Types.ObjectId, ref: 'SwapRequest', required: true },
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true },
+  text: { type: String },
+  fileUrl: { type: String },
+  fileName: { type: String },
+  fileType: { type: String },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Message', MessageSchema);
